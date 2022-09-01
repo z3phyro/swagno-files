2ed4cc7ba21db48cba5783912d2a3633b393dbb0
Anıl Şenay <anil.senay@cimri.com>
2022-09-01 15:05:19 +0300

    Move fileb0x file to root

--- fileb0x.go
+++ fileb0x.go
@@ -1,12 +1,13 @@
 package static
 
 import (
+	staticFiles "github.com/go-swagno/swagno-files/static"
 	"golang.org/x/net/webdav"
 )
 
 func NewHandler() *webdav.Handler {
 	return &webdav.Handler{
-		FileSystem: FS,
+		FileSystem: staticFiles.FS,
 		LockSystem: webdav.NewMemLS(),
 	}
 }
