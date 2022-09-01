d3ff9f5d12f3a3edaf9cac40ce3d5d690518d0b3
Anıl Şenay <anil.senay@cimri.com>
2022-09-01 15:11:42 +0300

    Remove static file and move files to root again

--- fileb0x.go
+++ fileb0x.go
@@ -1,13 +1,12 @@
 package static
 
 import (
-	staticFiles "github.com/go-swagno/swagno-files/static"
 	"golang.org/x/net/webdav"
 )
 
 func NewHandler() *webdav.Handler {
 	return &webdav.Handler{
-		FileSystem: staticFiles.FS,
+		FileSystem: FS,
 		LockSystem: webdav.NewMemLS(),
 	}
 }
